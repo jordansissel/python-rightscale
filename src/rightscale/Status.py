804497c784d6eaabe9553eb463db378339ee5e55
silver <silver@UA-MUK064.(none)>
2011-08-01 14:00:23 +0300

    Added docstring for Status class.

--- src/rightscale/Status.py
+++ src/rightscale/Status.py
@@ -2,6 +2,13 @@ from XMLModel import XMLModel
 from rightscale.util import ElementTreeValueOK
 
 class Status(XMLModel):
+    """ Provides a view of the status of any running jobs that you may have in
+        your account. The most common use of this resource is to check the
+        status of the execution of a RightScript.
+        
+        See http://support.rightscale.com/15-References/RightScale_API_Reference_Guide/02-Management/03-Statuses
+        for details.
+    """
     _state = None
     _description = None
     _href = None
